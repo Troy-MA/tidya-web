94b9e62a0b48c540b30edc7492edb56a8f6a07a7
Troy-MA <57745873+Troy-MA@users.noreply.github.com>
2022-10-16 01:35:27 +0200

    first commit

---
+++ src/components/sections/FeaturesSplit.js
@@ -0,0 +1,165 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionSplitProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
+
+const propTypes = {
+  ...SectionSplitProps.types,
+};
+
+const defaultProps = {
+  ...SectionSplitProps.defaults,
+};
+
+const FeaturesSplit = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "features-split section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "features-split-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  const splitClasses = classNames(
+    "split-wrap",
+    invertMobile && "invert-mobile",
+    invertDesktop && "invert-desktop",
+    alignTop && "align-top"
+  );
+
+  const sectionHeader = {
+    title: "Bringing the food to you",
+    paragraph:
+      "Looking for a food delivery service that can take your taste buds on a adventure? Then look no further! Our food delivery service offers a wide variety of cuisines to choose from, so you can always find something new to try. Plus, we deliver right to your door, so you can enjoy your meal in the comfort of your own home.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Bringing the food to you
+                </div>
+                <h3 className="mt-0 mb-12">Food for every Mood</h3>
+                <p className="m-0">
+                  Whatever you may be looking for, cuisines you are used to, the
+                  ones you've never had before — Adventure in Tidya, Discover
+                  new favorites.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/foods_dark.png")}
+                  alt="Features split 01"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-right"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Smooth sailing
+                </div>
+                <h3 className="mt-0 mb-12">Browse Menus with ease</h3>
+                <p className="m-0">
+                  Go through the menus with peace of mind, take your time to
+                  discover new tastes — Order at your own pace in your comfort
+                  zone
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/food_light_crop.png")}
+                  alt="Features split 02"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Deliveries
+                </div>
+                <h3 className="mt-0 mb-12">Delivery Right on you doorstep</h3>
+                <p className="m-0">
+                  Get your favorite meals at the comfort of your home, Order
+                  food through tidya and seat back while it gets delivered right
+                  on your doorstep.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/delivery.jpg")}
+                  alt="Features split 03"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
